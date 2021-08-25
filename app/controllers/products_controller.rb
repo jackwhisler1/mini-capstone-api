35e93e762880cb647bbafb679e1f4b9ea2ff109d
jack <whisler1@gmail.com>
2021-08-25 12:34:29 -0500

    adds individual book paths and methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,14 @@ class ProductsController < ApplicationController
     all_products = Product.all
     render json: all_products
   end
+
+  def mother_night_method
+    mother_night = Product.find(1)
+    render json: mother_night
+  end
+
+  def slaughterhousefive_method 
+    slaughterhouse_five = Product.find(2)
+    render json: slaughterhouse_five
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "all_products_method"
+
+  get "/mother_night", controller: "products", action: "mother_night_method"
+
+  get "/slaughterhouse_five", controller: "products", action: "slaughterhousefive_method"
 end
